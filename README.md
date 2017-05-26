b529cfb235ae0eb4e30de501eb47ce3a3e7b3988
Tim Jenness <tjenness@lsst.org>
2017-05-26 09:45:01 -0700

    Add latex MagicDraw template and fix up script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# LSE-61
-LSST Data Management System Requirements
+# LSE-61: LSST Data Management System Requirements
+
+Baselined versions of this document can be obtained from DocuShare at <https://ls.st/LSE-61>.
+
+This repository hosts reports from the LSE-61 MagicDraw model in LaTeX format.
+
+To generate a new version of the document from the model:
+
+* Upload the template file from the `support/` directory to MagicDraw using the _Report Wizard_.
+* Select the `LSE-61: Data Management System Requirements` part of the model within the _Report Wizard_.
+* Generate a `LSE-61.tex` file.
+* The output from MagicDraw requires some processing before it can be built using LaTeX. This mainly includes converting HTML elements to LaTeX equivalents.
+  Run the following command to do these fixes:
+```bash
+$ ./support/fixup.py /wherever/LSE-61.tex > LSE-61.tex
+```
+
+The resultant file should be buildable at this point.
+Check that it builds before committing it.
+The document requires the [lsst-texmf](https://lsst-texmf.lsst.io) LaTeX classes are available.
---
+++ support/fixup.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+import html
+
+"""
+Fix the contents of the TeX file created by MagicDraw using the requirements
+LaTeX template file to allow it to work with LaTeX.
+"""
+
+# Common regexes
+
+
+# Read in the specified file and write the updated version to standard out
+with open(sys.argv[1], "r") as fh:
+    in_table = False
+    in_list = False
+    for line in fh.readlines():
+        line = line.rstrip()
+
+        # Remove html pre wrappers. They can be ignored
+        line = re.sub(r"<html><pre>", "", line)
+        line = re.sub(r"</pre></html>", "", line)
+
+        # standalone open and close tags can be ignored
+        skip_line = False
+        for t in ("html", "head", "style", "body", "b"):
+            if skip_line:
+                break
+            if re.search(r"^\s*<{}>\s*$".format(t), line):
+                skip_line = True
+                break
+            if re.search(r"^\s*</{}>\s*$".format(t), line):
+                skip_line = True
+                break
+
+        # Style is irrelevant here
+        if re.search(r"padding:\d+px", line):
+            skip_line = True
+
+        # This line is not needed
+        if skip_line:
+            continue
+
+        # Remove pre tags
+        line = re.sub(r"<pre>", "", line)
+        line = re.sub(r"</pre>", "", line)
+
+        # A </b> at the start of a line can probably be removed
+        line = re.sub(r"^</b>", "", line)
+
+        # Fix the title to remove the document ref from the title. The Latex
+        # style handles the document ref itself and it should not be part
+        # of the title
+        if "title" in line:
+            line = re.sub(r"L\w\w\-\d+: ", "", line)
+
+        # Handle <ol> and <ul> tags
+        line = re.sub("<ol>", r"\\begin{enumerate}", line)
+        line = re.sub("</ol>", r"\\end{enumerate}", line)
+        line = re.sub("<ul>", r"\\begin{itemize}", line)
+        line = re.sub("</ul>", r"\\end{itemize}", line)
+
+        # Replace & with \& if this is a section (to void table confusion)
+        # May be easier to change to "and" in rth model
+        if re.search(r"section\{.* & ", line):
+            line = re.sub(r" & ", r" \& ", line)
+
+        # Handle style tabs for bold and italic
+        tags = {"b": "textbf", "i": "textit", "li": "item"}
+        for t in tags:
+            line = re.sub(r"<{0}>(.*?)</{0}>".format(t),
+                          r"\\{}{{\1}}".format(tags[t]), line)
+
+        # HTML Entity replacement
+        line = html.unescape(line)
+
+        # Parameter names need some help to be hyphenated properly
+        # if they are too long for the table. We look for case changes
+        # and hint to latex. The template makes this easy by using
+        # \paramname{} to tell us which are names
+        name = re.search(r"paramname{([A-Za-z0-9]+)}", line)
+        if name:
+            param = name.group(1)
+            outparam = param[0]
+            was_upper = outparam.isupper()
+            for c in param[1:]:
+                is_upper = c.isupper()
+                if is_upper and not was_upper:
+                    outparam += r"\-"
+                was_upper = is_upper
+                outparam += c
+            line = re.sub(param, outparam, line)
+
+        # Now that the line is fixed up, look for list
+        if re.match(r"\s*\* ", line):
+            if not in_table:
+                in_table = True
+                print("\\begin{itemize}")
+            line = re.sub(r"^\s*\*", r"\item", line)
+        elif in_table and re.match(r"^\s*$", line):
+            in_table = False
+            print("\\end{itemize}")
+
+        print(line)
