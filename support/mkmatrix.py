6c21a0fbca54f0978661f94c08d4f897894db6f0
Tim Jenness <tjenness@lsst.org>
2017-06-23 15:59:55 -0700

    Write out full doc to stdout and rows to files

--- support/mkmatrix.py
+++ support/mkmatrix.py
@@ -4,9 +4,14 @@ import sys
 import re
 
 """Read in the output from LSE-61-matrix.templ.txt template file and generate
-a latex long table"""
+a latex long table
 
-make_doc = True
+When the command runs, a full document is sent to standard output that can
+be compiled to verify the table cration, and two mapping files are written:
+DMS2OSS.tex and OSS2DMS.tex. These output files are the rows for a latex
+long table.
+"""
+
 dmsr2oss = {}
 oss2dmsr = {}
 titles = {}
@@ -44,21 +49,30 @@ def text2tex(title):
     return title
 
 
-def dumpdict(name1, name2, lut, reqtitles):
+def dumpdict(name1, name2, lut, reqtitles, fh=None, wrap=True):
     """Put dict of lists into longtable.
     name1 and name2 are the column names for table.
     name1 refers to the keys of lut and name2 to the values.
-    titles[] has the titles for each key in lut[].
+    reqtitles[] has the titles for each key in lut[].
+    fh is the file handle to write the table to.
+    Standard out is used if fh is None.
+    wrap controls whether the table wrapper is included in the output
+    or not. False, just writes the rows.
     """
 
-    print(r"""
+    if fh is None:
+        fh = sys.stdout
+
+    if wrap:
+        print(r"""
 \setlength\LTleft{0pt}
 \setlength\LTright{\fill}
 \begin{small}
 \begin{longtable}[]{|p{0.45\textwidth}|p{0.45\textwidth}@{}|}
-""")
-    print(r"""\hline \textbf{%s} & \textbf{%s} \\ \hline""" % (name1, name2))
-    print(r"""\endhead
+""", file=fh)
+        print(r"""\hline \textbf{%s} & \textbf{%s} \\ \hline""" %
+              (name1, name2), file=fh)
+        print(r"""\endhead
 
 \hline \multicolumn{2}{r}{\emph{Continued on next page}} \\
 \endfoot
@@ -66,7 +80,7 @@ def dumpdict(name1, name2, lut, reqtitles):
 \hline\hline
 \endlastfoot
 
-""")
+""", file=fh)
 
     def reqtext(reqid):
         return "{} {}".format(reqid, text2tex(reqtitles[reqid]))
@@ -79,31 +93,48 @@ def dumpdict(name1, name2, lut, reqtitles):
         # sometimes there is no tracing
         if lut[root]:
             for child in lut[root]:
-                print("{} &".format(basestr))
+                print("{} &".format(basestr), file=fh)
                 basestr = ""
-                print(r"{} \\".format(reqtext(child)))
+                print(r"{} \\".format(reqtext(child)), file=fh)
         else:
-            print(r"{} & \\".format(basestr))
-        print(r"\hline")
+            print(r"{} & \\".format(basestr), file=fh)
+        print(r"\hline", file=fh)
 
-    print(r"""
+    if wrap:
+        print(r"""
 \end{longtable}
 \end{small}
-""")
+""", file=fh)
 
 
-if make_doc:
-    print(r"""
+print(r"""
 \documentclass{article}
 \usepackage{longtable}
+\usepackage{geometry}
+\geometry{textheight=8.5in,
+          letterpaper,
+          textwidth=6.5in,
+          includeall,
+          lmargin=1in,
+          headheight=40pt,
+          headsep=2em,
+          marginparwidth=60pt,
+          footskip=40pt}
 \begin{document}
 """)
 
-# Write out the tables
-dumpdict("DMS", "OSS", dmsr2oss, titles)
-dumpdict("OSS", "DMS", oss2dmsr, titles)
+passes = (("DMS", "OSS", dmsr2oss),
+          ("OSS", "DMS", oss2dmsr))
 
-if make_doc:
-    print(r"""
+for p in passes:
+    # Write rows to the output file
+    outfile = "{p[0]}2{p[1]}.tex".format(p=p)
+    with open(outfile, "w") as outfh:
+        dumpdict(*p, titles, fh=outfh, wrap=False)
+
+    # Write to standard out
+    dumpdict(*p, titles, fh=None, wrap=True)
+
+print(r"""
 \end{document}
 """)
